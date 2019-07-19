3d7d4d06cb2e241c61f868706bbc6e8f0d6a7a11
Ednilson Cardoso Dos Santos <201700017232@COSIT-242151.ufs.internal>
2019-07-19 12:04:08 -0300

    aula criando o layout da tela principal

--- exemplo_youtube_curso/lib/screens/home.dart
+++ exemplo_youtube_curso/lib/screens/home.dart
@@ -4,6 +4,32 @@ import 'package:flutter/material.dart';
 class  Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar:AppBar(
+        title:Container(
+          height: 25 ,
+          child: Image.asset("images/youtube-logo.png"),
+        ) ,
+        elevation: 0,
+        backgroundColor: Colors.blue,
+        actions: <Widget>[
+          Align(
+            alignment:Alignment.center,
+            child:Text("0"),
+          ),
+          IconButton(icon:Icon(Icons.star) ,
+          onPressed: (){
+
+          },
+          ),
+          IconButton(icon:Icon(Icons.search) ,
+          onPressed: (){
+            
+          },
+          )
+        ],
+        ),
+        body: Container(),  
+    );
   }
 }
