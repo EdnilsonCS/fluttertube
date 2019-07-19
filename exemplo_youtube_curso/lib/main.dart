4c5b86e519792eae87277de6f07b41c0d8257500
Ednilson Cardoso Dos Santos <201700017232@COSIT-242151.ufs.internal>
2019-07-19 11:33:02 -0300

    Recendo lista de video no youtube

--- exemplo_youtube_curso/lib/main.dart
+++ exemplo_youtube_curso/lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:exemplo_youtube_curso/api.dart';
+import 'package:exemplo_youtube_curso/screens/home.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  Api api = Api();
+  api.search("eletro");
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
